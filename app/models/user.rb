df52ed6f0b0dd4650489791f6a442c691e23fcdc
anhcvt-2280 <vu.tuan.anhc@sun-asterisk.com>
2020-08-24 20:38:31 +0700

    Refs [TASK][27689] Log out

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,14 @@ class User < ApplicationRecord
             length: {maximum: Settings.validates.model.user.name.max_length}
   validates :email, presence: true,
             length: {maximum: Settings.validates.model.user.email.max_length},
-            uniqueness: {case_sensitive: true},
+            uniqueness: {case_sensitive: false},
             format: {with: VALID_EMAIL_REGEX}
   validates :password, presence: true,
             length: {minimum: Settings.validates.model.user.pwd.min_length},
             allow_nil: true
+
+  enum role: {trainee: Settings.roles.trainee, trainer: Settings.roles.trainer},
+       _prefix: true
 
   has_secure_password
 end
