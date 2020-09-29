bc7cd47a8043b581f41691fc745dc83b0fc578ec
anhnt-1044 <nguyen.trung.anh@sun-asterisk.com>
2020-09-29 11:42:07 +0700

    #29657 trainer filter by ransack

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # Assuming you have not yet modified this file, each configuration option below
 # is set to its default value. Note that some are commented out while others
 # are not: uncommented lines are intended to protect your configuration from
@@ -28,7 +26,6 @@ Devise.setup do |config|
 
   # Configure the class responsible to send e-mails.
   config.mailer = "UserMailer"
-
 
   # Configure the parent class responsible to send e-mails.
   # config.parent_mailer = 'ActionMailer::Base'
